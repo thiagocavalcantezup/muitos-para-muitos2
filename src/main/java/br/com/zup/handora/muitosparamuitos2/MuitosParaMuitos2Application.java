fa081aa2a4c10106a0b35529e7f14af1dc5a25a9
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-12 00:55:50 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/muitosparamuitos2/MuitosParaMuitos2Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.muitosparamuitos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuitosParaMuitos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuitosParaMuitos2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/handora/muitosparamuitos2/MuitosParaMuitos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.muitosparamuitos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuitosParaMuitos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
